eb6420427ce087d032482739d66b62a76729e05f
Taisei Nakajima <ne300085@senshu-u.jp>
2019-12-15 22:52:29 +0900

    complete get home-timeline

--- config/config.go
+++ config/config.go
@@ -1,9 +1,15 @@
 package config
 
-import "github.com/BurntSushi/toml"
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/dghubble/oauth1"
+	"net/http"
+
+	//"os"
+)
 
 type Config struct {
-	ConsumerToken string `toml:"consumerToken"`
+	ConsumerKey string `toml:"consumerKey"`
 	ConsumerSecret string `toml:"consumerSecret"`
 	AccessToken string `toml:"accessToken"`
 	AccessSecret string `toml:"accessSecret"`
@@ -11,9 +17,19 @@ type Config struct {
 
 var config Config
 
-func authTwitter() {
-	_, err := toml.DecodeFile("config.toml", config)
+func NewHttpClient() (*http.Client, error) {
+	_, err := toml.DecodeFile("config.toml", &config)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	oauthConfig := oauth1.NewConfig(config.ConsumerKey, config.ConsumerSecret)
+	oauthToken := oauth1.NewToken(config.AccessToken, config.AccessSecret)
+
+	/*** production ***/
+
+	//oauthConfig = oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
+	//oauthToken = oauth1.NewToken(os.Getenv("ACCESS_TOKEN"), os.Getenv("ACCESS_SECRET"))
+
+	httpClinet := oauthConfig.Client(oauth1.NoContext, oauthToken)
+	return httpClinet, nil
 }
--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/dghubble/go-twitter/twitter"
+	"gomoko/config"
+)
 
+func main() {
+	httpClient, err := config.NewHttpClient()
+	if err != nil {
+		panic(err)
+	}
+	client := twitter.NewClient(httpClient)
+
+	tweets, _, err := client.Timelines.HomeTimeline(&twitter.HomeTimelineParams{
+		Count: 20,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, tweet := range tweets {
+		fmt.Println(tweet.Text)
+	}
 }
 
