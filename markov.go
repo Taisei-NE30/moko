67404895494f61c2abc1f03b65b24581b5931478
Taisei Nakajima <ne300085@senshu-u.jp>
2019-12-17 16:00:04 +0900

    create generateTweetText

--- markov.go
+++ markov.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/mb-14/gomarkov"
+	"strings"
 )
 
 func NewChain(texts []string) *gomarkov.Chain {
@@ -11,4 +12,17 @@ func NewChain(texts []string) *gomarkov.Chain {
 		chain.Add(Tokenize(text))
 	}
 	return chain
+}
+
+func generateTweetText(chain *gomarkov.Chain) string {
+	order := chain.Order
+	tokens := make([]string, 0)
+	for i := 0; i < order; i++ {
+		tokens = append(tokens, gomarkov.StartToken)
+	}
+	for tokens[len(tokens)-1] != gomarkov.EndToken {
+		next, _ := chain.Generate(tokens[len(tokens)-order:])
+		tokens = append(tokens, next)
+	}
+	return strings.Join(tokens[order:len(tokens)-1], "")
 }
