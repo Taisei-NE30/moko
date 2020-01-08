e5d90d3810bfa63aed243bc7b3c5631f43cc1417
Taisei Nakajima <ne300085@senshu-u.jp>
2020-01-09 04:37:55 +0900

    create doMoko function

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"strconv"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -17,6 +18,13 @@ func main() {
 		panic(err)
 	}
 	client := twitter.NewClient(httpClient)
+	for {
+		doMoko(client)
+		time.Sleep(10 * time.Minute)
+	}
+}
+
+func doMoko(client *twitter.Client) {
 	wg := &sync.WaitGroup{}
 	chainCh := make(chan *gomarkov.Chain)
 	twCh := make(chan []twitter.Tweet)
